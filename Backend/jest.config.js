1e8da780ad46fdecbb8dc093e2c008333eb8bf75
MarcosPalacio <marcospalacio1994@gmail.com>
2026-01-29 21:45:21 -0300

    test: respect NODE_ENV to avoid starting server in Jest

--- Backend/jest.config.js
+++ Backend/jest.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.spec.ts'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/src/__tests__/helpers/',
+  ],
   verbose: true,
-  forceExit: true,
+  //forceExit: true,
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
-};
+  testTimeout: 30000,
+  setupFiles: ['<rootDir>/src/__tests__/setupEnv.ts'],
+}
